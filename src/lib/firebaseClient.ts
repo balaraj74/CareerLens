bc9356949fd1d2399004e2a29508287edbfa6ea4
Balaraj R <balarajr483@gmail.com>
2025-09-19 06:31:07 +0000

    our profile-page.tsx code looks fine, but the root cause of the client i

--- src/lib/firebaseClient.ts
+++ src/lib/firebaseClient.ts
@@ -6,13 +6,13 @@ import { getFirestore, initializeFirestore } from "firebase/firestore";
 import { getAnalytics, isSupported } from "firebase/analytics";
 
 const firebaseConfig: FirebaseOptions = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
+  apiKey: "AIzaSyAZRQLIieXFytt1ztD8uE6TeaqeT4ggBAs",
+  authDomain: "careerlens-1.firebaseapp.com",
+  projectId: "careerlens-1",
+  storageBucket: "careerlens-1.appspot.com",
+  messagingSenderId: "202306950137",
+  appId: "1:202306950137:web:ed4e91e619dd4cc7dde328",
+  measurementId: "G-WEF48JHJF9"
 };
 
 // Check for missing environment variables during build or server-side render
