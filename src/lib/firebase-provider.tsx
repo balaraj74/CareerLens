37adfb32b4fa4bef0e1d1b23fdea450d2168e843
Balaraj R <balarajr483@gmail.com>
2025-10-20 08:16:03 +0000

    const firebaseConfig = {   apiKey: "AIzaSyAZRQLIieXFytt1ztD8uE6TeaqeT4gg

--- src/lib/firebase-provider.tsx
+++ src/lib/firebase-provider.tsx
@@ -10,7 +10,7 @@ const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
   authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
   projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  databaseURL: `https://${process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID}.firebaseio.com`,
+  databaseURL: `https://careerlens-1.firebaseio.com`,
   storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
   appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
