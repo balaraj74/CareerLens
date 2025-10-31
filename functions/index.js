ed85eb3c6370b725507900f0085ca9843bf94c25
Balaraj R <balarajr483@gmail.com>
2025-10-31 11:28:49 +0000

    This issue occurs when Firebase Hosting cannot find a matching route in

---
+++ functions/index.js
@@ -0,0 +1,15 @@
+const functions = require("firebase-functions");
+const { onRequest } = require("firebase-functions/v2/https");
+const next = require("next");
+
+const nextjsServer = next({
+  dev: false,
+  conf: require("./.next/required-server-files.json"),
+});
+
+const handle = nextjsServer.getRequestHandler();
+
+exports.nextServer = onRequest(async (req, res) => {
+  await nextjsServer.prepare();
+  return handle(req, res);
+});
